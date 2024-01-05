9b74f7512b2ddfd4e670a6ab990ad5fc80b1079e
Ashta <33148617+ashtav@users.noreply.github.com>
2024-01-05 14:52:34 +0800

    widget testing

--- test/unit/widget_tests/login_test.dart
+++ test/unit/widget_tests/login_test.dart
@@ -1,42 +1,24 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:todo_app/app/screens/login/views/login_view.dart';
 
 void main() {
-  testWidgets('Successful login test', (WidgetTester tester) async {
-    final container = ProviderContainer();
-    addTearDown(container.dispose);
-
-    // final auth = container.read(authProvider.notifier);
-
-    // Membuat context sederhana menggunakan Builder
-    await tester.pumpWidget(
-      MaterialApp(
-        home: Builder(
-          builder: (BuildContext context) {
-            return GestureDetector(
-              onTap: () {
-                // auth.login2(context); // Memanggil login dengan context yang dibuat
-              },
-              child: const Text('Login'),
-            );
-          },
-        ),
+  testWidgets('Login screen', (WidgetTester tester) async {
+    await tester.pumpWidget(const ProviderScope(
+      child: MaterialApp(
+        home: LoginView(),
       ),
-    );
+    ));
+    await tester.pump();
 
-    // Meniru interaksi pengguna
-    // await tester.tap(find.text('Login'));
-    // await tester.pump();
+    // find LzButton with text "Login"
+    final loginButton = find.text('Login');
+    expect(loginButton, findsOneWidget);
 
-    // Verifikasi hasil setelah login
-    // expect(find.text('Logging in...'), findsOneWidget);
+    // tap the login button
+    await tester.tap(loginButton);
+    await tester.pump();
+    
   });
 }
